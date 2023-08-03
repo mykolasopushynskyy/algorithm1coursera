46918f0c97cfde312e165ed73b733214255eb0f8
Mykola Sopushynskyi <sopushynskyymykola@gmail.com>
2023-08-03 19:02:08 +0300

    MS: Added draft solution

--- src/main/java/Solver.java
+++ src/main/java/Solver.java
@@ -3,12 +3,13 @@ import edu.princeton.cs.algs4.MinPQ;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Solver {
 
     private final Board initial;
-    private final List<Board> solution;
-    private final MinPQ<SearchNode> nodeMinPQ;
+    private final MinPQ<SearchNode> movesPQ;
+    private List<Board> solution;
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
@@ -16,32 +17,32 @@ public class Solver {
             throw new IllegalArgumentException();
         }
 
+        this.solution = null;
         this.initial = initial;
-        this.solution = new ArrayList<>();
-        this.nodeMinPQ = new MinPQ<>(manhattan());
+        this.movesPQ = new MinPQ<>(manhattanPriority());
         solve();
     }
 
     // is the initial board solvable? (see below)
     public boolean isSolvable() {
-        return solution.size() == 0;
+        return solution != null;
     }
 
     // min number of moves to solve initial board; -1 if unsolvable
     public int moves() {
-        return solution.size() == 0 ? -1 : solution.size();
+        return solution == null ? -1 : solution.size();
     }
 
     // sequence of boards in a shortest solution; null if unsolvable
     public Iterable<Board> solution() {
-        return solution.size() == 0 ? null : new ArrayList<>(solution);
+        return solution == null ? null : new ArrayList<>(solution);
     }
 
-    private static Comparator<SearchNode> manhattan() {
+    private static Comparator<SearchNode> manhattanPriority() {
         return Comparator.comparingInt(node -> node.count + node.board.manhattan());
     }
 
-    private static Comparator<SearchNode> hamming() {
+    private static Comparator<SearchNode> hammingPriority() {
         return Comparator.comparingInt(node -> node.count + node.board.hamming());
     }
 
@@ -58,8 +59,33 @@ public class Solver {
     }
 
     private void solve() {
-        nodeMinPQ.insert(new SearchNode(initial, null));
+        boolean shouldSearch;
+        movesPQ.insert(new SearchNode(initial, null));
+        SearchNode move = movesPQ.delMin();
 
+        while (!move.board.isGoal()) {
+            shouldSearch = false;
+            for (Board nextBoard : move.board.neighbors()) {
+                if (move.previous != null && !nextBoard.equals(move.previous.board)) {
+                    movesPQ.insert(new SearchNode(nextBoard, move));
+                    shouldSearch = true;
+                }
+            }
+            // dead end
+            if (!shouldSearch) {
+                return;
+            }
+            move = movesPQ.delMin();
+        }
+
+        // convert to boards
+        List<SearchNode> solution = new ArrayList<>()
+        while (move.previous != null) {
+            solution.add(move);
+            move = move.previous;
+        }
+        solution.sort(Comparator.comparing(m -> m.count));
+        this.solution = solution.stream().map(m -> m.board).collect(Collectors.toList());
     }
 
 
